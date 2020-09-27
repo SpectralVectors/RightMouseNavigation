dceb7a6ec73d85e79bb3888064a70e1607f667f8
SpectralVectors <robmuir1@gmail.com>
2020-09-27 16:59:59 -0400

    3rd Revision to squash bugs
    First version had the issue where the Walk Navigation modal operator would continue to run in the background when the context menu was called.
    
    This was resolved by moving all logic into the modal loop, and using a Boolean flag to exit the loop when certain conditions had been met.
    
    Tested in Object, Edit Mesh - Vertex, Edge, Face, Weight Paint, Texture Paint and Vertex Modes. Further testing needed to ensure compatibility with Pose Mode, etc
    
    Also cleaned up the menu function, which is where bugs would lie extending it to other modes.
    
    Code is commented with explanations for any lines that didn't speak for themselves.

--- UnrealRightClick.py
+++ UnrealRightClick.py
@@ -11,40 +11,47 @@ class UnrealRightClick(bpy.types.Operator):
     _count = 0
     _threshold = 0.3
     MOUSE_RIGHTUP = 0x0010
+    _finished = False
 
     def modal(self, context, event):
+        # The _finished Boolean acts as a flag to exit the modal loop, it is not made True until after the cancel function is called
+        if self._finished == True:
+            return{'CANCELLED'}
         if context.space_data.type == 'VIEW_3D':
                 if event.type in {'RIGHTMOUSE'}: 
                     if event.value in {'RELEASE'}:
+                        # This fakes a Right Mouse Up event using Ctypes
+                        ctypes.windll.user32.mouse_event(self.MOUSE_RIGHTUP)
+                        # This brings back our mouse cursor to use with the menu
+                        context.window.cursor_modal_restore()
+                        # If the length of time you've been holding down Right Mouse is longer than the threshold value, then try to call a context menu, and if that fails, call a context panel
+                        if self._count < self._threshold:
+                            try:
+                                bpy.ops.wm.call_menu(name="VIEW3D_MT_"+context.mode.lower()+"_context_menu")
+                            except:
+                                bpy.ops.wm.call_panel(name="VIEW3D_PT_"+context.mode.lower()+"_context_menu")
                         self.cancel(context)
-                        return {'CANCELLED'}
+                        # We now set the flag to true to exit the modal operator on the next loop through
+                        self._finished = True
+                        return {'PASS_THROUGH'}
 
                 if event.type == 'TIMER':
                     self._count += 0.01
                 return {'PASS_THROUGH'}
 
     def execute(self, context):
-        bpy.ops.view3d.walk('INVOKE_REGION_WIN')
+        # Execute is the first thing called in our operator, so we start by
+        # calling Blender's built-in Walk Navigation
+        bpy.ops.view3d.walk('INVOKE_DEFAULT')
         wm = context.window_manager
+        # Adding the timer and starting the loop
         self._timer = wm.event_timer_add(0.1, window=context.window)
         wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
     def cancel(self, context):
         wm = context.window_manager
-        wm.event_timer_remove(self._timer)
-        ctypes.windll.user32.mouse_event(self.MOUSE_RIGHTUP)
-        self.menu(context)
-        
-    def menu(self, context):     
-        context.window.cursor_modal_set('DEFAULT')
-        if self._count < self._threshold:
-            try:
-                bpy.ops.wm.call_menu(name="VIEW3D_MT_"+context.mode.lower()
-                +"_context_menu")
-            except:
-                bpy.ops.wm.call_panel(name="VIEW3D_PT_"+context.mode.lower()
-                +"_context_menu")
+        wm.event_timer_remove(self._timer)     
             
 def register():
     bpy.utils.register_class(UnrealRightClick)
