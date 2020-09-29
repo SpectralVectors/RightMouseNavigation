13c963dea16e9bbf0fc0629a0112f06987e08b3b
SpectralVectors <robmuir1@gmail.com>
2020-09-29 11:33:42 -0400

    Cleaned up Register/Unregister keymap
    Added a couple of loops, cut the file size in half, much neater code now, and should work without issues.
    
    I notice that the "View3D Walk Modal" map says 'Restore' after removing the add-on, but when I check it out it seems to be back to its default state, so I'm just going to ignore that for now ;P

--- UnrealRightClick.py
+++ UnrealRightClick.py
@@ -122,90 +122,33 @@ def register_keymaps():
         kmi.active = True
         addon_keymaps.append((km, kmi))
 
-        for key in wm.keyconfigs.user.keymaps["Object Mode"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Mesh"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Curve"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Armature"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Metaball"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Lattice"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Font"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Pose"].keymap_items:
-            if (
-                key.idname == "wm.call_menu"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Vertex Paint"].keymap_items:
-            if (
-                key.idname == "wm.call_panel"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Weight Paint"].keymap_items:
-            if (
-                key.idname == "wm.call_panel"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Image Paint"].keymap_items:
-            if (
-                key.idname == "wm.call_panel"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
-        for key in wm.keyconfigs.user.keymaps["Sculpt"].keymap_items:
-            if (
-                key.idname == "wm.call_panel"
-                and key.type == "RIGHTMOUSE"
-                and key.active
-            ):
-                key.active = False
+        menumodes = ["Object Mode", "Mesh", "Curve", "Armature", "Metaball", "Lattice", "Font", "Pose"]
+        panelmodes = ["Vertex Paint", "Weight Paint", "Image Paint", "Sculpt"]
+        
+        # These Modes all call standard menus
+        # "Object Mode", "Mesh", "Curve", "Armature", "Metaball", "Lattice",
+        # "Font", "Pose"
+        for i in menumodes:
+            for key in wm.keyconfigs.user.keymaps[i].keymap_items:
+                if (
+                    key.idname == "wm.call_menu"
+                    and key.type == "RIGHTMOUSE"
+                    and key.active
+                ):
+                    key.active = False
+
+        # These Modes call panels instead of menus
+        # "Vertex Paint", "Weight Paint", "Image Paint", "Sculpt"
+        for i in panelmodes:
+            for key in wm.keyconfigs.user.keymaps[i].keymap_items:
+                if (
+                    key.idname == "wm.call_panel"
+                    and key.type == "RIGHTMOUSE"
+                    and key.active
+                ):
+                    key.active = False
+
+        # Changing the Walk Modal Map        
         for key in wm.keyconfigs.user.keymaps["View3D Walk Modal"].keymap_items:
             if (
                 key.propvalue == "CANCEL"
@@ -236,78 +179,31 @@ def unregister():
             wm.keyconfigs.addon.keymaps.remove(km)
     addon_keymaps.clear()
 
-    for key in wm.keyconfigs.user.keymaps["Object Mode"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Mesh"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Curve"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Armature"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Metaball"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Lattice"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Font"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Pose"].keymap_items:
-        if (
-            key.idname == "wm.call_menu"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Vertex Paint"].keymap_items:
-        if (
-            key.idname == "wm.call_panel"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Weight Paint"].keymap_items:
-        if (
-            key.idname == "wm.call_panel"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Image Paint"].keymap_items:
-        if (
-            key.idname == "wm.call_panel"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
-    for key in wm.keyconfigs.user.keymaps["Sculpt"].keymap_items:
-        if (
-            key.idname == "wm.call_panel"
-            and key.type == "RIGHTMOUSE"
-        ):
-            key.active = True
+    menumodes = ["Object Mode", "Mesh", "Curve", "Armature", "Metaball", "Lattice", "Font", "Pose"]
+    panelmodes = ["Vertex Paint", "Weight Paint", "Image Paint", "Sculpt"]
+    
+    # Reactivating menus
+    # "Object Mode", "Mesh", "Curve", "Armature", "Metaball", "Lattice",
+    # "Font", "Pose"
+    for i in menumodes:
+            for key in wm.keyconfigs.user.keymaps[i].keymap_items:
+                if (
+                    key.idname == "wm.call_menu"
+                    and key.type == "RIGHTMOUSE"
+                ):
+                    key.active = True
+
+    # Reactivating panels
+    # "Vertex Paint", "Weight Paint", "Image Paint", "Sculpt"    
+    for i in panelmodes:
+            for key in wm.keyconfigs.user.keymaps[i].keymap_items:
+                if (
+                    key.idname == "wm.call_panel"
+                    and key.type == "RIGHTMOUSE"
+                ):
+                    key.active = True
+
+    # Changing the Walk Modal Map back     
     for key in wm.keyconfigs.user.keymaps["View3D Walk Modal"].keymap_items:
         if (
             key.propvalue == "CANCEL"
@@ -321,5 +217,6 @@ def unregister():
         ):
             key.type = "LEFTMOUSE"
             key.value = "PRESS"
+
 if __name__ == "__main__":
     register()
